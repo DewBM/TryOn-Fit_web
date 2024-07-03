fd2d0654981ff92765773181bd7e31b6e45f57c6
GRPS GANEGODA <piumisarangag@gmail.com>
2024-07-03 12:47:50 +0530

    creating the navigation bar

--- try-on_fit/app/page.tsx
+++ try-on_fit/app/page.tsx
@@ -1,8 +1,14 @@
+'use client'
 import Image from "next/image";
 
+import NavBar from "./components/NavBar";
+import { useState } from "react";
+
 export default function Home() {
+ 
   return (
     <>
+     
     </>
   );
 }
