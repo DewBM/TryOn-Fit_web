0a3c05a3f9ab6063af7b924758a4ffffdb99af85
DewBM <dewmitha.mahiti@gmail.com>
2024-07-10 14:21:26 +0530

    fix issues when merging branch.

--- try-on_fit/app/page.tsx
+++ try-on_fit/app/page.tsx
@@ -1,26 +1,24 @@
-'use client'
-import Image from "next/image";
-import Footer from "./components/Footer"
-import NavBar from "./components/NavBar";
-import Card from "./components/Product"
+//import NavBar from "./components/NavBar";
 import EmpNavBar from "./components/EmpNavBar";
 import { useState } from "react";
 import Header from "./components/Header/index";
+import NavBar from "./components/NavBar";
+import { Card } from "@nextui-org/react";
 
 export default function Home() {
   return (
-<>
-    <div>
-      <NavBar />
-      <div className="  h-[100vh]">
-      <div className=" flex flex-row flex-wrap pr-[20px]  pt-[25vh] gap-3 pl-[40px]">
- 
-      <div><Card /></div>
-      <div><Card /></div> 
-      <div><Card /></div>
-      <div><Card /></div> 
-    
-    </div>
-</>
+    <>
+      <div>
+        <NavBar />
+        <div className="  h-[100vh]">
+          <div className=" flex flex-row flex-wrap pr-[20px]  pt-[25vh] gap-3 pl-[40px]">
+            <div><Card /></div>
+            <div><Card /></div> 
+            <div><Card /></div>
+            <div><Card /></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
