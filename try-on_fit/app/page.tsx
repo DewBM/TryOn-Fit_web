c16e7e8e59e4d80f55402daee1b5d3525c826536
GRPS GANEGODA <piumisarangag@gmail.com>
2024-07-10 18:16:42 +0530

    correct errors in product card

--- try-on_fit/app/page.tsx
+++ try-on_fit/app/page.tsx
@@ -6,21 +6,24 @@ import Card from "./components/Product"
 import EmpNavBar from "./components/EmpNavBar";
 import { useState } from "react";
 import Header from "./components/Header/index";
+import Product from "./components/Product"
 
 export default function Home() {
   return (
-<>
+
     <div>
       <NavBar />
       <div className="  h-[100vh]">
       <div className=" flex flex-row flex-wrap pr-[20px]  pt-[25vh] gap-3 pl-[40px]">
- 
-      <div><Card /></div>
-      <div><Card /></div> 
-      <div><Card /></div>
-      <div><Card /></div> 
-    
+     <div><Product/></div>
+     <div><Product/></div>
+     <div><Product/></div>
+     <div><Product/></div>
+     <div><Product/></div>
+     
     </div>
-</>
+    </div>
+    </div>
+
   );
 }
