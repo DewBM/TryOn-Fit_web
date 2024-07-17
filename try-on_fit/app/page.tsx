e73bd3bc5d0bc33a3c7494ddf06fb00f520f9a4c
GRPS GANEGODA <piumisarangag@gmail.com>
2024-07-17 08:05:58 +0530

    Adding the Product Page

--- try-on_fit/app/page.tsx
+++ try-on_fit/app/page.tsx
@@ -1,4 +1,5 @@
 //import NavBar from "./components/NavBar";
+
 import EmpNavBar from "./components/EmpNavBar";
 import { useState } from "react";
 import Header from "./components/Header/index";
@@ -6,11 +7,14 @@ import Sidebar from "./components/SideBar";
 import NavBar from "./components/NavBar";
 import Product from "./components/Product"
 import Slider from "./components/Slider";
+import ProductPage from "./components/Productpage";
 
 export default function Home() {
   return (
 
-   <></>
+   <div>
+   <ProductPage />
+    </div>
 
   );
 }
