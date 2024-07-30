e994983efbb5c2c8d3e9d8dd0382b02c90b612c3
GRPS GANEGODA <piumisarangag@gmail.com>
2024-07-30 10:43:04 +0530

    Create a status page

---
+++ try-on_fit/app/(Webstore)/status/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '@/app/components/NavBar'
+import Footer from '@/app/components/Footer'
+function page() {
+  return (
+    <div>
+        <NavBar/>
+        
+        <Footer/>
+    </div>
+  )
+}
+
+export default page
