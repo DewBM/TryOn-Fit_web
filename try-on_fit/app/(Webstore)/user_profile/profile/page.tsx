e99f2e03da7aeeb7450f136e43ad757d95080868
sapuni dheerasinha <122308654+sapunethmini@users.noreply.github.com>
2024-07-31 10:02:33 +0530

    add user profile page

--- try-on_fit/app/(Webstore)/user_profile/profile/page.tsx
+++ try-on_fit/app/(Webstore)/user_profile/profile/page.tsx
@@ -1,3 +1,40 @@
+import Button from "@/app/components/Button";
+import PasswordBox from "@/app/components/PasswordBox";
+import Link from "next/link";
+import TextBox from "@/app/components/TextBox";
+import Image from "next/image";
+import { useFormState } from "react-dom";
+import { LoginSchema } from "@/app/utils/schema";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import React from "react";
+import Form from "@/app/components/Form";
 
+function page() {
+  return (
+    <div className="grid bg-main-lighter lg:grid-cols-12 mt-4 max-w-xl mb-0 sm:grid-cols-6 rounded mx-8 ">
+      <div className="lg:col-span-4 ">
+        <Image
+          src="/images/profile_pic.webp"
+          alt=""
+          width={100}
+          height={100}
+          className=" rounded-l-lg shadow-xl border-1 lg:col-start-1 "
+        />
+      </div>
+      <div className="lg:col-span-7 lg:col-start-5">
+        <div className="lg:col-span-7 ">My Profile</div>
+        <TextBox
+          labelName="Full Name"
+          name="Full_name"
+          inputType="text"
+          key="F_name"
+          defaultValue="Matheesha Pathirana"
+          disabled={true}
+        />
+      </div>
+    </div>
+  );
+}
 
-
+export default page;
--- try-on_fit/app/components/TextBox.tsx
+++ try-on_fit/app/components/TextBox.tsx
@@ -8,6 +8,7 @@ export default function TextBox({
   key,
   defaultValue,
   placeholder = null,
+  disabled = false,
 }: {
   labelName: String;
   // id: String;
@@ -16,6 +17,7 @@ export default function TextBox({
   key: React.Key;
   defaultValue: React.HTMLInputTypeAttribute;
   placeholder?: string | null;
+  disabled?: boolean;
 }) {
   return (
     <div>
@@ -33,6 +35,7 @@ export default function TextBox({
           key={key}
           defaultValue={defaultValue}
           placeholder={placeholder ?? ""}
+          disabled={disabled}
           className="block w-full h-9 rounded-md border-0 focus:outline-none mt py-1.5 pl-4 text-gray-900 ring-1 ring-inset ring-main-light focus:ring-2 focus:ring-inset focus:ring-main-dark sm:text-sm sm:leading-6"
         />
       </div>
