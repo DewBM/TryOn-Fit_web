876fe6f4ac7f07a6b99d494e8d0dc5ababf1402a
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-08-01 20:45:21 +0530

    Sidebar navigation.

--- try-on_fit/app/(employee)/StoreManager/employee/emp_add.tsx
+++ try-on_fit/app/(employee)/StoreManager/employee/emp_add.tsx
@@ -23,7 +23,9 @@ const EmpAddForm = ({
   const [form, fields] = useForm({
     lastResult,
     onValidate({ formData }) {
-      const result = parseWithZod(formData, { schema: EmployeeRegistrationSchema });
+      const result = parseWithZod(formData, {
+        schema: EmployeeRegistrationSchema,
+      });
       console.log(result);
       return result;
     },
@@ -39,155 +41,220 @@ const EmpAddForm = ({
     };
     if (isOpen) {
       document.addEventListener("mousedown", handleClickOutside);
-      document.body.classList.add("h-screen", "overflow-hidden");
+      document.body.classList.add("overflow-hidden");
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
-      document.body.classList.remove("h-screen", "overflow-hidden");
+      document.body.classList.remove("overflow-hidden");
     }
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
-      document.body.classList.remove("h-screen", "overflow-hidden");
+      document.body.classList.remove("overflow-hidden");
     };
   }, [isOpen, onClose]);
 
   const [selectedValue, setSelectedValue] = useState("");
 
   return (
-    <dialog
-      ref={dialogRef}
-      id="dialog"
-      open={isOpen}
-      className="z-50 bg-white shadow-md"
-    >
-      <div className="lg:col-span-6 lg:col-start-4 rounded bg-slate-50 shadow-xl rounded-r-lg pt-6 pb-8 mt-10 mb-10">
-        <form 
-          id={form.id}
-          onSubmit={form.onSubmit}
-          action={action}
-          noValidate
-          className="lg:col-span-5 sm:col-span-4"
+    <>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-40"
+          style={{ backdropFilter: "blur(8px)" }}
         >
-          <div className="grid grid-cols-11">
-            <div className="lg:col-span-7 lg:col-start-2 sm:col-span-4 mt-2 text-2xl font-extrabold mb-4">
-              <h1>Employee Registration</h1>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
-              <TextBox
-                labelName="First Name"
-                name={fields.firstName.name}
-                inputType="text"
-                key={fields.firstName.key as React.Key}
-                defaultValue={fields.firstName.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter First Name"
-              />
-              <div className="text-xs text-red-400">{fields.firstName.errors}</div>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-7 sm:col-span-1 mt-3">
-              <TextBox
-                labelName="Last Name"
-                name={fields.lastName.name}
-                inputType="text"
-                key={fields.lastName.key as React.Key}
-                defaultValue={fields.lastName.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter Last Name"
-              />
-              <div className="text-xs text-red-400">{fields.lastName.errors}</div>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
-              <TextBox
-                labelName="Email"
-                name={fields.email.name}
-                inputType="email"
-                key={fields.email.key as React.Key}
-                defaultValue={fields.email.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter Email"
-              />
-              <div className="text-xs text-red-400">{fields.email.errors}</div>
-            </div>
-            
-            <div className="lg:col-span-4 lg:col-start-7 sm:col-span-1 mt-3">
-              <TextBox
-                labelName="Phone"
-                name={fields.phone.name}
-                inputType="tel"
-                key={fields.phone.key as React.Key}
-                defaultValue={fields.phone.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter Phone Number"
-              />
-              <div className="text-xs text-red-400">{fields.phone.errors}</div>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
-              <SelectBox
-                labelName="Employee Role"
-                id="signup-role"
-                name={fields.employeeRole.name} // Ensure the name matches the schema
-                options={[
-                  { value: "", label: "Select Role" },
-                  { value: "CusSupport", label: "Customer Support" },
-                  { value: "DisCoordinator", label: "Distribution Coordinator" },
-                  { value: "Stockkeeper", label: "Stockkeeper" },
-                ]}
-                autoComplete="role"
-                value={selectedValue}
-                onChange={(newValue: React.SetStateAction<string>) => setSelectedValue(newValue)} // Add this line
-              />
-              <div className="text-xs text-red-400">{fields.employeeRole.errors}</div>
-            </div>
-            <div className="lg:col-span-5 lg:col-start-7 sm:col-span-1 mt-3">
-              <fieldset>
-                <legend className="block text-sm font-medium leading-6 text-gray-900">
-                  Gender
-                </legend>
-                <div  style={{ display: "flex", flexWrap: "nowrap", alignItems: "center" }}>
-                  <RadioButton label="Male" value="male" name={fields.gender.name} />
-                  <RadioButton label="Female" value="female" name={fields.gender.name} />
-                  <RadioButton label="Other" value="other" name={fields.gender.name} />
+          {/* Dark overlay with blur effect */}
+        </div>
+      )}
+      <dialog
+        ref={dialogRef}
+        id="dialog"
+        open={isOpen}
+        className="z-50 bg-white shadow-md rounded"
+        style={{ backdropFilter: "none" }} // Ensure the form is not blurred
+      >
+        <div className="lg:col-span-6 lg:col-start-4 rounded bg-slate-50 shadow-xl rounded-r-lg pt-4 pb-8 mt-2 mb-2">
+          <form
+            id={form.id}
+            onSubmit={form.onSubmit}
+            action={action}
+            noValidate
+            className="lg:col-span-5 sm:col-span-4"
+          >
+            <div className="grid grid-cols-11">
+              <div className="lg:col-span-7 lg:col-start-2 sm:col-span-4 mt-2 text-2xl font-extrabold mb-4">
+                <h1>Employee Registration</h1>
+              </div>
+              <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
+                <TextBox
+                  labelName="First Name"
+                  name={fields.firstName.name}
+                  inputType="text"
+                  key={fields.firstName.key as React.Key}
+                  defaultValue={
+                    fields.firstName
+                      .initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter First Name"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.firstName.errors}
                 </div>
-              </fieldset>
-              {/* <div className="text-xs text-red-400">{fields.username.errors}</div> */}
+              </div>
+              <div className="lg:col-span-4 lg:col-start-7 sm:col-span-1 mt-3">
+                <TextBox
+                  labelName="Last Name"
+                  name={fields.lastName.name}
+                  inputType="text"
+                  key={fields.lastName.key as React.Key}
+                  defaultValue={
+                    fields.lastName.initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter Last Name"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.lastName.errors}
+                </div>
+              </div>
+              <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
+                <TextBox
+                  labelName="Email"
+                  name={fields.email.name}
+                  inputType="email"
+                  key={fields.email.key as React.Key}
+                  defaultValue={
+                    fields.email.initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter Email"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.email.errors}
+                </div>
+              </div>
+
+              <div className="lg:col-span-4 lg:col-start-7 sm:col-span-1 mt-3">
+                <TextBox
+                  labelName="Phone"
+                  name={fields.phone.name}
+                  inputType="tel"
+                  key={fields.phone.key as React.Key}
+                  defaultValue={
+                    fields.phone.initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter Phone Number"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.phone.errors}
+                </div>
+              </div>
+              <div className="lg:col-span-4 lg:col-start-2 sm:col-span-1 mt-3">
+                <SelectBox
+                  labelName="Employee Role"
+                  id="signup-role"
+                  name={fields.employeeRole.name} // Ensure the name matches the schema
+                  options={[
+                    { value: "", label: "Select Role" },
+                    { value: "CusSupport", label: "Customer Support" },
+                    {
+                      value: "DisCoordinator",
+                      label: "Distribution Coordinator",
+                    },
+                    { value: "Stockkeeper", label: "Stockkeeper" },
+                  ]}
+                  autoComplete="role"
+                  value={selectedValue}
+                  onChange={(newValue: React.SetStateAction<string>) =>
+                    setSelectedValue(newValue)
+                  } // Add this line
+                />
+                <div className="text-xs text-red-400">
+                  {fields.employeeRole.errors}
+                </div>
+              </div>
+              <div className="lg:col-span-5 lg:col-start-7 sm:col-span-1 mt-3">
+                <fieldset>
+                  <legend className="block text-sm font-medium leading-6 text-gray-900">
+                    Gender
+                  </legend>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexWrap: "nowrap",
+                      alignItems: "center",
+                    }}
+                  >
+                    <RadioButton
+                      label="Male"
+                      value="male"
+                      name={fields.gender.name}
+                    />
+                    <RadioButton
+                      label="Female"
+                      value="female"
+                      name={fields.gender.name}
+                    />
+                    <RadioButton
+                      label="Other"
+                      value="other"
+                      name={fields.gender.name}
+                    />
+                  </div>
+                </fieldset>
+                {/* <div className="text-xs text-red-400">{fields.username.errors}</div> */}
+              </div>
+              <div className="lg:col-span-9 lg:col-start-2 sm:col-span-4 mt-3">
+                <TextBox
+                  labelName="Street Address"
+                  name={fields.streetAddress.name}
+                  inputType="text"
+                  key={fields.streetAddress.key as React.Key}
+                  defaultValue={
+                    fields.streetAddress
+                      .initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter Street Address"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.streetAddress.errors}
+                </div>
+              </div>
+              <div className="lg:col-span-4 lg:col-start-2 sm:col-span-2 mt-3">
+                <TextBox
+                  labelName="City"
+                  name={fields.city.name}
+                  inputType="text"
+                  key={fields.city.key as React.Key}
+                  defaultValue={
+                    fields.city.initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter City"
+                />
+                <div className="text-xs text-red-400">{fields.city.errors}</div>
+              </div>
+              <div className="lg:col-span-4 lg:col-start-7 sm:col-span-2 mt-3">
+                <TextBox
+                  labelName="State/Province"
+                  name={fields.stateProvince.name}
+                  inputType="text"
+                  key={fields.stateProvince.key as React.Key}
+                  defaultValue={
+                    fields.stateProvince
+                      .initialValue as React.HTMLInputTypeAttribute
+                  }
+                  placeholder="Enter State/Province"
+                />
+                <div className="text-xs text-red-400">
+                  {fields.stateProvince.errors}
+                </div>
+              </div>
+              <div className="lg:col-span-7 lg:col-start-3 sm:col-span-4 mt-3">
+                <Button type="submit" className="py-1.5 px-28 ml-10">
+                  Register
+                </Button>
+              </div>
             </div>
-            <div className="lg:col-span-9 lg:col-start-2 sm:col-span-4 mt-3">
-              <TextBox
-                labelName="Street Address"
-                name={fields.streetAddress.name}
-                inputType="text"
-                key={fields.streetAddress.key as React.Key}
-                defaultValue={fields.streetAddress.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter Street Address"
-              />
-              <div className="text-xs text-red-400">{fields.streetAddress.errors}</div>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-2 sm:col-span-2 mt-3">
-              <TextBox
-                labelName="City"
-                name={fields.city.name}
-                inputType="text"
-                key={fields.city.key as React.Key}
-                defaultValue={fields.city.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter City"
-              />
-              <div className="text-xs text-red-400">{fields.city.errors}</div>
-            </div>
-            <div className="lg:col-span-4 lg:col-start-7 sm:col-span-2 mt-3">
-              <TextBox
-                labelName="State/Province"
-                name={fields.stateProvince.name}
-                inputType="text"
-                key={fields.stateProvince.key as React.Key}
-                defaultValue={fields.stateProvince.initialValue as React.HTMLInputTypeAttribute}
-                placeholder="Enter State/Province"
-              />
-              <div className="text-xs text-red-400">{fields.stateProvince.errors}</div>
-            </div>
-            <div className="lg:col-span-7 lg:col-start-3 sm:col-span-4 mt-3">
-              <Button type="submit" className="py-1.5 px-28 ml-10">
-                Register
-              </Button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </dialog>
+          </form>
+        </div>
+      </dialog>
+    </>
   );
 };
 
--- try-on_fit/app/components/SideBar/index.tsx
+++ try-on_fit/app/components/SideBar/index.tsx
@@ -14,19 +14,23 @@ const Sidebar = (props: {
 
   const sidebarItems = [
     { label: "DashBoard", route: "/", icon: <FiHome /> },
-    { label: "Products", route: "/about", icon: <FiShoppingBag /> },
-    { label: "Catergories", route: "/notifications", icon: <FiTag /> },
+    { label: "Products", route: "../StockKeeper/Product", icon: <FiShoppingBag /> }, // Adjusted to match the folder structure
+    { label: "Employees", route: "../StoreManager/employee", icon: <FiTag /> },
+    { label: "Supplier", route: "../StoreManager/supplier", icon: <FiTag /> },
     { label: "Settings", route: "/messages", icon: <FiSettings /> },
   ];
 
   return (
     <aside
-      className={`fixed inset-y-0 left-0 z-50 w-64 bg-main-lighter shadow-md transition-transform transform   ${
+      className={`fixed inset-y-0 left-0 z-50 w-64 bg-main-lighter shadow-md transition-transform transform ${
         props.sideBarOpen ? "translate-x-0" : "-translate-x-full sm:ml-0"
       } lg:translate-x-0`}
     >
       <div className="items-center">
-        <img src="/images/Fashion_logo.svg" className="h-18 w-18 align-middle px-12  mt-6 mb-3" />
+        <img
+          src="/images/Fashion_logo.svg"
+          className="h-18 w-18 align-middle px-12 mt-6 mb-3"
+        />
       </div>
       <nav className="p-5">
         <ul className="space-y-4">
