635faca25e1b1e806f82952c89544a2ff87d9cdc
GRPS GANEGODA <piumisarangag@gmail.com>
2024-08-01 20:24:32 +0530

    remove the add to cart and add to fit on buttons from the product card

--- try-on_fit/app/components/Product.tsx
+++ try-on_fit/app/components/Product.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import {Card, CardHeader, CardBody, Image ,Button} from "@nextui-org/react";
 import image from "next/image"
 import ItemCard from "./Helper/ItemCard"
-export default function Product(){
+interface props{
+  image:string[];
+  name:string[];
+  price:number[];
+}
+export default function Product({image,name,price}:props){
   return(
     <Card className=" w-80  py-4 bg-main-lighter flex justify-center ">
      
@@ -19,12 +24,12 @@ export default function Product(){
       <CardHeader className="pb-0 pt-0 px-4 flex-col items-center">
         {/* // ading props for that  */}
         <ItemCard name="Brown Summer Coat" price="$70000"/>
-       <div className=" pt-5 flex flex-row gap-10">
+       {/* <div className=" pt-5 flex flex-row gap-10">
         
 
         <button className=" flex rounded-2xl border border-main-dark strock-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light"> Add to Fiton</button>
         <button className=" flex rounded-2xl border border-main-dark strock-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light"> Add to Cart</button>
-       </div>
+       </div> */}
        </CardHeader>
     </Card>
   
