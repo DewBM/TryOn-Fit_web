6f52be3644443f6227308c8d6f41cce2b2074060
GRPS GANEGODA <piumisarangag@gmail.com>
2024-08-01 22:28:25 +0530

    minor changes done

--- try-on_fit/app/components/Product.tsx
+++ try-on_fit/app/components/Product.tsx
@@ -1,38 +1,40 @@
-"use client"
+"use client"; // Indicates this is a client-side component
+
 import React from "react";
-import {Card, CardHeader, CardBody, Image ,Button} from "@nextui-org/react";
-import image from "next/image"
-import ItemCard from "./Helper/ItemCard"
-interface props{
-  image:string[];
-  name:string[];
-  price:number[];
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import Image from "next/image"; // Correct Image import from next/image
+import ItemCard from "./Helper/ItemCard";
+
+interface Props {
+  images: string[]; // Changed to an array of strings
+  name: string;
+  price: number;
 }
-export default function Product({image,name,price}:props){
-  return(
-    <Card className=" w-80  py-4 bg-main-lighter flex justify-center ">
-     
-      <CardBody className="overflow-visible pt-2">
-        <Image
-          alt="Card background"
-          className="object-contain rounded-lg pl-2"
-          // ading img props
-          src="images/pic.png"
-          style={{ width: '270px', height: '200px' }}
-        />
+
+const Product: React.FC<Props> = ({ images, name, price }) => {
+  return (
+    <Card className="w-40 py-4 bg-main-lighter flex justify-center">
+      <CardBody className="overflow-visible justify-center items-center pt-2">
+        {images.map((imgSrc, index) => (
+          <Image
+            key={index}
+            alt={`Card background ${index}`}
+            className="object-contain rounded-2xl"
+            src={imgSrc}
+            width={100} // Set the width
+            height={100} 
+          />
+        ))}
       </CardBody>
       <CardHeader className="pb-0 pt-0 px-4 flex-col items-center">
-        {/* // ading props for that  */}
-        <ItemCard name="Brown Summer Coat" price="$70000"/>
-       {/* <div className=" pt-5 flex flex-row gap-10">
-        
-
-        <button className=" flex rounded-2xl border border-main-dark strock-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light"> Add to Fiton</button>
-        <button className=" flex rounded-2xl border border-main-dark strock-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light"> Add to Cart</button>
-       </div> */}
-       </CardHeader>
+        <ItemCard  name={name} price={price} />
+        {/* <div className="pt-5 flex flex-row gap-10">
+          <button className="flex rounded-2xl border border-main-dark stroke-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light">Add to Fiton</button>
+          <button className="flex rounded-2xl border border-main-dark stroke-1 px-2 py-1 justify-start cursor-pointer text-xs hover:bg-main-light">Add to Cart</button>
+        </div> */}
+      </CardHeader>
     </Card>
-  
   );
-}
+};
 
+export default Product;
