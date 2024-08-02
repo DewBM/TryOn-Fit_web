ac9008cb224c57606962f1d275c2885035cd475e
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-08-02 06:59:23 +0530

    Add view for product.

--- try-on_fit/app/(employee)/StockKeeper/Product/view_product/page.tsx
+++ try-on_fit/app/(employee)/StockKeeper/Product/view_product/page.tsx
@@ -1,20 +1,71 @@
 "use client";
 import React from "react";
-import TextBox from "@/app/components/TextBox";
-import TextB_Dsble from "@/app/components/TextB_Dsble";
+import ProductDetails from "@/app/components/ProductDetail";
+import SizeColorTable from "@/app/components/SizeColorTable";
+import SizesTable from "@/app/components/T-shirtSizesTable";
 
-export default function ViewProduct() {
+const ProductPage = () => {
+  const productInfo = {
+    product_id: "1234",
+    supplier_id: "S01",
+    product_name: "red frock",
+    catergory: "Frock",
+    quantity: "49",
+    description: "Korean style t-shirt",
+    colours: "red",
+    gender: "Female",
+    age_category: "Adult",
+    image:"./images/red.png",
+  };
+
   return (
-    <div className="container mx-auto p-2">
-      <h1 className="text-lg font-semibold mb-5">Product details</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
-        <div className="sm:col-span-1 border p-4"></div>
-        <div className="sm:col-span-3 border p-4">
-          <div className="text-md font-semibold">General Information </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3"></div>
-          <div className="bg-main  lg:col-span-4 sm:col-span-2 lg:col-start-7 mb-4 "></div>
+    <div className="container mx-auto p-0">
+      <h1 className="text-lg font-semibold mb-5">Product Details</h1>
+      <div className="grid grid-cols-[1fr_3fr] gap-4">
+        <div className="border rounded-md">
+        <img
+        src="/images/red.png"
+        className="w-full h-auto rounded-md mt-12"
+      />
+        </div>
+        <div className="border rounded-md">
+          <div className="grid grid-cols-2 gap-2 p-6">
+            <ProductDetails label="Product ID" value={productInfo.product_id} />
+            <ProductDetails
+              label="Supplier ID"
+              value={productInfo.supplier_id}
+            />
+            <ProductDetails
+              label="Product Name"
+              value={productInfo.product_name}
+            />
+            <ProductDetails label="Catergory" value={productInfo.catergory} />
+            <ProductDetails label="Quantity" value={productInfo.quantity} />
+            <ProductDetails label="Colour" value={productInfo.colours} />
+            <ProductDetails label="Gender" value={productInfo.gender} />
+            <ProductDetails
+              label="Age Category"
+              value={productInfo.age_category}
+            />
+          </div>
+          <div className=" ml-7 w-100% mb-2">
+            <ProductDetails
+              label="Description"
+              value={productInfo.description}
+            />
+          </div>
         </div>
       </div>
+      <h1 className="text-md font-semibold m-5">
+        Available sizes,Colours and Quantity
+      </h1>
+      <div className="border rounded-md mt-2 justify-center items-center">
+        <SizeColorTable />
+      </div>
+      <h1 className="text-md font-semibold m-5">Measurements and sizes</h1>
+      <div className="border rounded-md mt-2 justify-center items-center"><SizesTable /></div>
     </div>
   );
-}
+};
+
+export default ProductPage;
