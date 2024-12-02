9f3cafac1d534274a1a55e637c9296f26d62100c
pehan <pehanrathnayake159@gmail.com>
2024-12-02 20:05:51 +0530

    reports Page

--- try-on_fit/app/(employee)/StoreManager/reports/page.tsx
+++ try-on_fit/app/(employee)/StoreManager/reports/page.tsx
@@ -40,6 +40,7 @@ export default function Home() {
         </h1>
         <p className="mt-2 text-gray-500 dark:text-gray-400">
           Track sales, revenue, orders, and returns with ease.
+          
         </p>
       </div>
 
