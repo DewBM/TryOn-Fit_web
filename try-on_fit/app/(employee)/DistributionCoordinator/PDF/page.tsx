83b8bf56fb7e12c91a0cd0ec0c5d4db3da5bf6b3
GRPS GANEGODA <piumisarangag@gmail.com>
2024-12-03 19:22:55 +0530

    add the status page

--- try-on_fit/app/(employee)/DistributionCoordinator/PDF/page.tsx
+++ try-on_fit/app/(employee)/DistributionCoordinator/PDF/page.tsx
@@ -5,7 +5,13 @@ import  { useRouter } from 'next/navigation';
 import SupportAgentRoundedIcon from '@mui/icons-material/SupportAgentRounded';
 import Image from 'next/image';
 
+interface Props{
+    orderId:string ;
+   
+    status:string;
 
+
+}
 type OrderSummary = {
   subtotal: number;
   delivery: number;
@@ -22,7 +28,7 @@ type Checkout = {
   quantity: number;
 };
 
-function InvoicePage() {
+function PDFPage({orderId,status}:Props) {
   const router = useRouter();
   const [cartItems, setCartItems] = useState<Checkout[]>([]);
   const [orderSummary, setOrderSummary] = useState<OrderSummary>({
@@ -65,16 +71,21 @@ function InvoicePage() {
       </div>
       <div className="border rounded-lg shadow-lg bg-white">
         <div className="p-8" id="invoice">
-          <h1 className="text-3xl font-bold text-center text-gray-700 mb-8">Invoice</h1>
-          <div className="flex justify-center mb-8 ">
+          <h1 className="text-3xl font-bold text-center text-gray-700 mb-8">Status of the Order Items </h1>
+          <div className="flex  justify-center mb-8 ">
         
-            <div className="text-center">
-          
-              <h2 className="font-bold text-lg">TryOnFit</h2>
-              <p>No.66/A, Kaduwela Road, Battaramulla</p>
-              <p>Western, Sri Lanka</p>
-              <p>+94 11 123 456</p>
-              <p>TryOnFit.customercare@gmail.com</p>
+            <div className="text-left">
+               <div className="flex flex-row">
+               <h2 className="font-semibold text-lg">Order Id:</h2>
+               <h2 className="font-semibold text-lg">{orderId}</h2>
+               </div>
+               
+              
+               <div className="flex flex-row">
+               <h2 className="font-semibold text-lg">Stats:</h2>
+               <h2 className="font-semibold text-lg">{status}</h2>
+               </div>
+             
             </div>
           </div>
           <table className="w-full border-collapse border border-gray-300">
@@ -83,6 +94,9 @@ function InvoicePage() {
                 <th className="border border-gray-300 px-4 py-2">#</th>
                 <th className="border border-gray-300 px-4 py-2 text-left">DESCRIPTION</th>
                 <th className="border border-gray-300 px-4 py-2 text-right">UNIT PRICE</th>
+                <th className="border border-gray-300 px-4 py-2 text-right">size</th>
+                <th className="border border-gray-300 px-4 py-2 text-right">varient color</th>
+                <th className="border border-gray-300 px-4 py-2 text-right">Gender</th>
                 <th className="border border-gray-300 px-4 py-2 text-right">QUANTITY</th>
                 <th className="border border-gray-300 px-4 py-2 text-right">TOTAL</th>
               </tr>
@@ -92,33 +106,38 @@ function InvoicePage() {
                 <tr key={item.id}>
                   <td className="border border-gray-300 px-4 py-2 text-center">{index + 1}</td>
                   <td className="border border-gray-300 px-4 py-2">{item.title}</td>
-                  <td className="border border-gray-300 px-4 py-2 text-right">
-                    Rs. {item.price.toFixed(2)}
-                  </td>
                   <td className="border border-gray-300 px-4 py-2 text-right">{item.quantity}</td>
+                  <td className="border border-gray-300 px-4 py-2 text-right">lg</td>
+                  
+                  <td className="border border-gray-300 px-4 py-2 text-right">red</td>
+                  <td className="border border-gray-300 px-4 py-2 text-right">femail</td>
                   <td className="border border-gray-300 px-4 py-2 text-right">
                     Rs. {(item.price * item.quantity).toFixed(2)}
                   </td>
+                  <td className="border border-gray-300 px-4 py-2 text-right">
+                    Rs. {item.price.toFixed(2)}
+                  </td>
+                 
                 </tr>
               ))}
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan={3}></td>
+                <td colSpan={6}></td>
                 <td className="border border-gray-300 px-4 py-2 text-right font-bold">SUBTOTAL</td>
                 <td className="border border-gray-300 px-4 py-2 text-right">
                   Rs. {orderSummary.subtotal.toFixed(2)}
                 </td>
               </tr>
               <tr>
-                <td colSpan={3}></td>
+                <td colSpan={6}></td>
                 <td className="border border-gray-300 px-4 py-2 text-right font-bold">DELIVERY</td>
                 <td className="border border-gray-300 px-4 py-2 text-right">
                   Rs. {orderSummary.delivery.toFixed(2)}
                 </td>
               </tr>
               <tr>
-                <td colSpan={3}></td>
+                <td colSpan={6}></td>
                 <td className="border border-gray-300 px-4 py-2 text-right font-bold">GRAND TOTAL</td>
                 <td className="border border-gray-300 px-4 py-2 text-right">
                   Rs. {orderSummary.total.toFixed(2)}
@@ -128,10 +147,6 @@ function InvoicePage() {
           </table>
           <div className="mt-8 text-center">
             <p className="font-bold">Thank you!</p>
-            <p className="text-sm text-gray-500">
-              Further Informations Contact Our Help Center.</p>
-
-              <button className='align-middle border bg-main-dark font-bold text-white  px-8 py-4 rounded-lg ' onClick={handlecustomer}><span className='p-4'><SupportAgentRoundedIcon/></span>Help Center</button>
         
           </div>
         </div>
@@ -140,4 +155,4 @@ function InvoicePage() {
   );
 }
 
-export default InvoicePage;
+export default PDFPage;
